b83c308afbebe3f5f81240c0dfcab06751731938
Jeffrey Immanuel <94883101+Jeffmanjones@users.noreply.github.com>
2022-02-15 03:52:35 +0000

    p3

--- p3final.c
+++ p3final.c
@@ -8,5 +8,24 @@ int input_n()
 }
 int sum_n(int n)
 {
-  
+  int sum=0;
+  for(int i=0;i<=n;i++)
+    sum+=i;
+  return sum;
+}
+void output(int n,int sum)
+{
+  for(int i=1;i<n;i++)
+  {
+    printf("%d+",i);
+  }
+  printf("%d is %d",n,sum);
+}
+int main()
+{
+  int n,sum;
+  n=input_n();
+  sum=sum_n(n);
+  output(n,sum);
+  return 0;
 }
